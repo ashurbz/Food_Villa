59c5ff77ac1da0de0401a68ba98b829a92d72eef
Ashish <rbzashu1@gmail.com>
2023-04-11 14:39:55 +0530

    Created sibling using react.create element

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
-const heading =React.createElement('h1',{id:"heading"},"Hello World from React");
+// const heading =React.createElement('h1',{id:"heading"},"Hello World from React");
 
-    const root = ReactDOM.createRoot(document.getElementById('root'));
+//     const root = ReactDOM.createRoot(document.getElementById('root'));
 
-    root.render(heading);
+//     root.render(heading);
+
+// <div id='parent' >
+//     <div id='child'>
+//         <h1>Hello H1 Tag</h1>
+//         <h2>Hello H2 Tag</h2>
+//     </div>
+// </div>
+
+// Creating using react
+
+const parent =React.createElement('div' ,{id:'parent'},
+    React.createElement('div' ,{id:'child'},
+    React.createElement('h1',{}, 'Hello H1 Tag'), React.createElement('h2',{},'Hello H2 tag')
+    )
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
